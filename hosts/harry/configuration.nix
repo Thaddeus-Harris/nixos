26bb60d687d5a1712086c5616b3b9fcb61675035
Thaddeus-Harris <thaddeusyharris@gmail.com>
2025-12-10 17:56:31 -0500

    Initial Commit

---
+++ hosts/harry/configuration.nix
@@ -0,0 +1,149 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+      ../../modules/theming/stylix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enables Hardware Features
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  security.rtkit.enable = true;
+  security.pam.services.yprlock = {};
+  security.doas.enable = true;
+  # Enable networking features 
+  networking.networkmanager.enable = true;
+
+  #Environment Variables 
+   environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enabled experimental features
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tyh = {
+    isNormalUser = true;
+    description = "TYH";
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
+    packages = with pkgs; [];
+  };
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "tyh" = import ./home.nix;
+      };
+  };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  neofetch wofi swww bluez blueman brightnessctl alsa-utils keepassxc git home-manager nautilus python314 busybox lact
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+programs.hyprland.enable = true;
+programs.steam.enable = true;
+programs.steam.gamescopeSession.enable = true;
+programs.gamemode.enable = true;
+
+  # List services that you want to enable:
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd hyprland";
+	user = "greeter";
+	};
+      };
+  };
+  services.redshift = {
+	  enable = true;
+	  temperature = {
+		  day = 5700;
+		  night = 3500;
+	  };
+  };
+  services.blueman.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable=true;
+    };
+  services.printing.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+location.latitude = 42.1;
+location.longitude = -71.1;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ modules/theming/stylix/default.nix
@@ -0,0 +1,36 @@
+{ inputs, pkgs, lib, ...}:
+
+let
+  theme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+in
+{
+  stylix = {
+    enable = true;
+    base16Scheme = theme;
+    image = ./gruvbox_mountain_range.jpg;
+    opacity = {
+      terminal = 0.9;
+      popups = 0.9;
+      desktop = 0.9;
+    };
+    cursor = {
+      package = pkgs.capitaine-cursors-themed;
+      name = "Capitaine Cursors (Gruvbox)";
+      size = 20;
+    };
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+	name = "JetBrains Mono";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+	name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+	name = "DejaVu Serif";
+      };
+    };
+  };
+}
